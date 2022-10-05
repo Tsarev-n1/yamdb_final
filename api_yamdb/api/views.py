66a7cf8f62f40be0e7051b9cd3a7eeaa3f6fb4a5
Михаил Царев <caruov@yandex.ru>
2022-10-05 21:08:23 +0300

    Flake8 fixes

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,7 +7,6 @@ from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
-
 from reviews.models import Category, Genre, Review, Title, User, UserAuth
 
 from .filters import TitleFilter
