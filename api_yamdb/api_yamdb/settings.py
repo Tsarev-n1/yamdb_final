17607c69591fb01ff5253405a0b004cac4c5cdac
Михаил Царев <caruov@yandex.ru>
2022-10-05 20:18:29 +0300

    Fixed env

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,8 +1,10 @@
 import os
 from datetime import timedelta
+from dotenv import load_dotenv
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+load_dotenv('.\infra\.env')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
