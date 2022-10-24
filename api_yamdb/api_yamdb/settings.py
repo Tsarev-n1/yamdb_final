c9211a1b3368850adb5e1bbc13f07d9bf6f2dc8c
Михаил Царев <caruov@yandex.ru>
2022-10-24 20:58:44 +0300

    Fixes

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 load_dotenv()
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', default='123456')
 
 DEBUG = False
 
